10bb497b67d50ba2112982b0e6886168fc9bf870
satorukano <kiritomo0206@icloud.com>
2023-01-13 21:09:04 +0900

    [add] add update function

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTION"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: "3306",
@@ -16,24 +27,44 @@ const connection = mysql.createConnection({
 connection.connect()
 
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   const id = req.body.id
   const table = req.body.table
   let list = require("./module/crew_list")
   let crew_list = list.item
-  connection.query(`SELECT * FROM crew WHERE id = 1`, (error, results) => {
+  connection.query('SELECT * FROM crew WHERE id = ?', [id], (error, results) => {
     let data = results[0]
     let count = 1
     data = Object.values(data)
-    console.log(data)
     crew_list.forEach((list) => {
       list.check_list.forEach((item) => {
         item.check = data[count]
         count++
       })
     }) 
-    res.setHeader('Access-Control-Allow-Origin', '*')
     res.send(crew_list)
+  })
+})
+
+app.get('/member', (req, res) => {
+  connection.query('SELECT * FROM name', (error, results) => {
+    let data = results[0]
+    res.send([data])
+  })
+})
+
+app.post('/edit', (req, res) => {
+  const id = req.body.id
+  const table = req.body.table
+  const data = req.body.data
+  data.forEach((list) => {
+    list.check_list.forEach((item) => {
+      connection.query('UPDATE crew SET ?? = ? WHERE id = 1', [item.id, item.check], (error, results) => {
+        if (error) {
+          res.send("error")
+        }
+      })
+    })
   })
 })
 
